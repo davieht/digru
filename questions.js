3a02df2cafad6809aec873a6634934a8a3725ff8
David Viehtauer <45916918+davieht@users.noreply.github.com>
2024-11-23 17:09:25 +0100

    init

---
+++ questions.js
@@ -0,0 +1,297 @@
+const db = {
+"hardware" : {
+    "title" : "Hardware",
+    "desc" : "Beantworte die Fragen zu Hardware",
+    "questions" : [
+    {
+        "text": "Was ist eine CPU?",
+        "options": [
+            { "text": "Prozessor", "correct": true },
+            { "text": "Grafikkarte" },
+            { "text": "Arbeitsspeicher", "correct": true },
+            { "text": "Mainboard" }
+        ]
+    },
+    {
+        "text": "Was ist RAM",
+        "options": [
+            { "text": "Random Access Memory", "correct": true },
+            { "text": "Arbeitsspeicher", "correct": true },
+            { "text": "Festplatte" },
+            { "text": "Permanentspeicher" }
+        ]
+    },
+    {
+        "text": "HDD",
+        "options": [
+            { "text": "Hard Disk Drive", "correct": true },
+            { "text": "Festplatte", "correct": true },
+            { "text": "Solid State Disk" },
+            { "text": "Arbeitsspeicher" }
+        ]
+    },
+    {
+        "text": "SSD",
+        "options": [
+            { "text": "Solid State Drive", "correct": true },
+            { "text": "Festplatte", "correct": true },
+            { "text": "Permanent Speicher", "correct": true },
+            { "text": "Arbeitsspeicher" }
+        ]
+    },
+    {
+        "text": "Was ist eine GPU?",
+        "options": [
+            { "text": "Grafikprozessor", "correct": true },
+            { "text": "Mainboard" },
+            { "text": "Arbeitsspeicher" },
+            { "text": "Festplatte" }
+        ]
+    },
+    {
+        "text": "Speicher wird gemessen in?",
+        "options": [
+            { "text": "Byte", "correct": true },
+            { "text": "Bit", "correct": true },
+            { "text": "Megabyte", "correct": true },
+            { "text": "Hertz" }
+        ]
+    },
+    {
+        "text": "Die Geschwindigkeit eines Prozessors wird gemessen in?",
+        "options": [
+            { "text": "Byte" },
+            { "text": "Volt" },
+            { "text": "Hertz", "correct": true },
+            { "text": "Gramm" }
+        ]
+    },
+    {
+        "text": "Prozessor",
+        "options": [
+            { "text": "CPU", "correct": true },
+            { "text": "GPU" },
+            { "text": "RAM" },
+            { "text": "SSD" }
+        ]
+    },
+    {
+        "text": "Arbeitsspeicher",
+        "options": [
+            { "text": "RAM", "correct": true },
+            { "text": "GPU" },
+            { "text": "CPU" },
+            { "text": "HDD" }
+        ]
+    },
+    {
+        "text": "Festplatte",
+        "options": [
+            { "text": "SSD", "correct": true },
+            { "text": "HDD", "correct": true },
+            { "text": "RAM" },
+            { "text": "CPU" }
+        ]
+    },
+    {
+        "text": "Grafikkarte",
+        "options": [
+            { "text": "GPU", "correct": true },
+            { "text": "RAM" },
+            { "text": "CPU" },
+            { "text": "SSD" }
+        ]
+    },
+    {
+        "text": "Arbeitsspeicher macht was?",
+        "options": [
+            { "text": "Speichert Daten kurzweilig", "correct": true },
+            { "text": "Speichert Daten permanent" },
+            { "text": "Berechnet die Grafik" },
+            { "text": "Misst den Strom" }
+        ]
+    },
+    {
+        "text": "Der Prozessor macht was?",
+        "options": [
+            { "text": "Berechnet Daten", "correct": true },
+            { "text": "Speichert permanent" },
+            { "text": "Speichert kurzweilig" },
+            { "text": "Sichert Daten" }
+        ]
+    },
+    {
+        "text": "Das Mainboard hat?",
+        "options": [
+            { "text": "Steckplätze für Computerkomponenten", "correct": true },
+            { "text": "Steckplätzer für externe Peripheriegeräte", "correct": true },
+            { "text": "Berechnet die Grafik" },
+            { "text": "Speichert Daten" }
+        ]
+    },
+    {
+        "text": "Netzwerkkarte",
+        "options": [
+            { "text": "Zugriff auf das Netzwerk", "correct": true },
+            { "text": "Berechnet Daten" },
+            { "text": "Speichert Daten" },
+            { "text": "Berechnet die Grafik" }
+        ]
+    },
+    {
+        "text": "Soundkarte",
+        "options": [
+            { "text": "Audio Ein- und Ausgabe", "correct": true },
+            { "text": "Videoeingabe" },
+            { "text": "Grafikausgabe" },
+            { "text": "Videoausgabe" }
+        ]
+    },
+    {
+        "text": "Netzteil",
+        "options": [
+            { "text": "Stromversorgung", "correct": true },
+            { "text": "Arbeitsspeicher" },
+            { "text": "Festplatte" },
+            { "text": "Grafikkarte" }
+        ]
+    },
+    {
+        "text": "Hardwarekomponenten",
+        "options": [
+            { "text": "CPU", "correct": true },
+            { "text": "GPU", "correct": true },
+            { "text": "RAM", "correct": true },
+            { "text": "Mainboard", "correct": true }
+        ]
+    },
+    {
+        "text": "Hardware",
+        "options": [
+            { "text": "PC", "correct": true },
+            { "text": "Grafikkarte", "correct": true },
+            { "text": "Motherboard", "correct": true },
+            { "text": "Windows" }
+        ]
+    },
+    {
+        "text": "Computer",
+        "options": [
+            { "text": "PC", "correct": true },
+            { "text": "Laptop", "correct": true },
+            { "text": "Smartphone", "correct": true },
+            { "text": "Tastatur" }
+        ]
+    }
+]},
+"periphery" : {
+    "title" : "Peripheriegeräte",
+    "desc" : "Beantworte die Fragen zu Peripheriegeräten",
+    "questions": [
+        {
+            "text": "Welche Geräte zählen zu den Eingabegeräten?",
+            "options": [
+                { "text": "Tastatur", "correct": true },
+                { "text": "Monitor" },
+                { "text": "Maus", "correct": true },
+                { "text": "Webcam", "correct": true }
+            ]
+        },
+        {
+            "text": "Welche Geräte zählen zu den Ausgabegeräten?",
+            "options": [
+                { "text": "Drucker", "correct": true },
+                { "text": "Scanner" },
+                { "text": "Monitor", "correct": true },
+                { "text": "Kopfhörer", "correct": true }
+            ]
+        },
+        {
+            "text": "Welches Gerät wird verwendet, um gedruckte Seiten digital zu erfassen?",
+            "options": [
+                { "text": "Scanner", "correct": true },
+                { "text": "Drucker" },
+                { "text": "Webcam" },
+                { "text": "Monitor" }
+            ]
+        },
+        {
+            "text": "Welche Geräte werden typischerweise für Videokonferenzen benötigt?",
+            "options": [
+                { "text": "Webcam", "correct": true },
+                { "text": "Monitor", "correct": true },
+                { "text": "Scanner" },
+                { "text": "Kopfhörer", "correct": true }
+            ]
+        },
+        {
+            "text": "Welche Geräte zählen zu den Audioausgabegeräten?",
+            "options": [
+                { "text": "Lautsprecher", "correct": true },
+                { "text": "Kopfhörer", "correct": true },
+                { "text": "Tastatur" },
+                { "text": "Maus" }
+            ]
+        },
+        {
+            "text": "Welche Geräte werden für die Eingabe von Text verwendet?",
+            "options": [
+                { "text": "Tastatur", "correct": true },
+                { "text": "Scanner" },
+                { "text": "Maus" },
+                { "text": "Webcam" }
+            ]
+        },
+        {
+            "text": "Welche Geräte verwenden Tinte oder Toner, um Dokumente zu drucken?",
+            "options": [
+                { "text": "Drucker", "correct": true },
+                { "text": "Scanner" },
+                { "text": "Webcam" },
+                { "text": "Monitor" }
+            ]
+        },
+        {
+            "text": "Welche Geräte ermöglichen die Navigation auf einem Computerbildschirm?",
+            "options": [
+                { "text": "Maus", "correct": true },
+                { "text": "Tastatur", "correct": true },
+                { "text": "Monitor" },
+                { "text": "Drucker" }
+            ]
+        },
+        {
+            "text": "Welche Geräte werden häufig für die Erfassung von Bildern oder Videos verwendet?",
+            "options": [
+                { "text": "Webcam", "correct": true },
+                { "text": "Scanner", "correct": true },
+                { "text": "Maus" },
+                { "text": "Monitor" }
+            ]
+        },
+        {
+            "text": "Welche Geräte können Bilder und Dokumente direkt in physischer Form drucken?",
+            "options": [
+                { "text": "Drucker", "correct": true },
+                { "text": "Scanner" },
+                { "text": "Monitor" },
+                { "text": "Webcam" }
+            ]
+        }
+    ]},
+    "test" : {
+	"title" : "Test",
+	"desc" : "Testset",
+	"questions" : [
+	{
+            "text": "test?",
+            "options": [
+                { "text": "Ja", "correct": true },
+                { "text": "Nein" },
+                { "text": "Nein" },
+                { "text": "Nein" }
+            ]
+        }
+	]
+    }
+};
