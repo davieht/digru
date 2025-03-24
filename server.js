276f697aaf9ec2536e6fa300027951936e47690d
daveloper <david.viehtauer@gmail.com>
2025-03-24 14:36:07 +0100

    fixed new pw and comment localhost start

--- server.js
+++ server.js
@@ -157,16 +157,17 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-
-//const options = {
-//  key: fs.readFileSync("/etc/letsencrypt/live/digru.at/privkey.pem"),
-//  cert: fs.readFileSync("/etc/letsencrypt/live/digru.at/fullchain.pem"),
-//};
-
-//https.createServer(options, app).listen(443, () => {
-//  console.log("HTTPS Server running on port 443");
+// Start the server on localhost
+//app.listen(PORT, () => {
+//    console.log(`Server running on http://localhost:${PORT}`);
 //});
+
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/digru.at/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/digru.at/fullchain.pem"),
+};
+
+// Start the server
+https.createServer(options, app).listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});
